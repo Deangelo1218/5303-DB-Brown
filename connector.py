5b0e3502d3ad727f5485147561caaae4c6b710ea
Deangelo Brown <Deangelobrown24218@gmail.com>
2021-09-22 00:05:01 +0000

    Creating Database with python connector.

--- connector.py
+++ connector.py
@@ -7,6 +7,8 @@ try:
         user=input("Enter username: "),
         password=getpass("Enter password: "),
     ) as connection:
-        print(connection)
+        create_db_query = "CREATE DATABASE online_movie_rating"
+        with connection.cursor() as cursor:
+            cursor.execute(create_db_query)
 except Error as e:
     print(e)
