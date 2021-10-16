f38888d47ad6b4c01f66e6f66040eef77362e736
Deangelo Brown <Deangelobrown24218@gmail.com>
2021-10-16 17:59:31 +0000

    Updated main.py.

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 # main.py
 
+from typing import Optional
 from fastapi import FastAPI
 import json 
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+class Item(BaseModel):
+    name: str
+    description: str
+    price: float
+    tax: float 
+
+@app.post("/items")
+async def create_item(item: Item):
+    return Item
